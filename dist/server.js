4e298dac15e534f300efebd7804e4e04de163b4d
André Minoro Fusioka <minorofusioka@hotmail.com>
2018-04-18 19:19:28 -0300

    redirecionamento http para https

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var path = require('path');
+var secure = require('ssl-express-www');
 var serveStatic = require('serve-static');
 
 app = express();
 app.use(serveStatic(__dirname));
+app.use(secure);
 
 var port = process.env.PORT || 5000;
 app.listen(port);
