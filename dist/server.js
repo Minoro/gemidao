bad286b0af75cd0f5b6746f53c3d32454142ebf6
André Minoro Fusioka <minorofusioka@hotmail.com>
2018-04-19 19:16:15 -0300

    Pacote para redirecionar o http para https

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var path = require('path');
-var secure = require('ssl-express-www');
+var httpsRedirect = require('express-https-redirect');
 var serveStatic = require('serve-static');
 
 app = express();
 app.use(serveStatic(__dirname));
-app.use(secure);
+app.use('/', httpsRedirect(true));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
